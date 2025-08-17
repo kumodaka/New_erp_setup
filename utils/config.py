6c2ed7a911f685e2b9eb5075b34005107b6152e1
kumodaka <kumodaka@gmail.com>
2025-08-17 18:19:19 +0530

    Adding demo config

--- utils/config.py
+++ utils/config.py
@@ -4,16 +4,16 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    # host: str = os.getenv("DB_HOST")
-    # port: int = int(os.getenv("DB_PORT", 5432))
-    # database: str = os.getenv("DB_NAME")
-    # user: str = os.getenv("DB_USER")
-    # password: str = os.getenv("DB_PASSWORD")
-    host: str = "localhost"
-    port: int = 5432
-    database: str = "epr"
-    user: str = "postgres"
-    password: str = "12345678"
+    host: str = os.getenv("DB_HOST")
+    port: int = int(os.getenv("DB_PORT", 5432))
+    database: str = os.getenv("DB_NAME")
+    user: str = os.getenv("DB_USER")
+    password: str = os.getenv("DB_PASSWORD")
+    # host: str = "localhost"
+    # port: int = 5432
+    # database: str = "epr"
+    # user: str = "postgres"
+    # password: str = "12345678"
 
     def __post_init__(self):
         print(f"DEBUG: Config initialized with host={self.host}, port={self.port}, database={self.database}, user={self.user}")
